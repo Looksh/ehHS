c0339f9c76cf0aa042f9daf53dfe8140d8c18090
Looksh <rlatmdgus720680@gmail.com>
2018-08-02 23:27:51 +0900

    은행고 앱 UI를 CardView로 교체

--- app/src/main/java/moonu/com/ehhs/ehMain.java
+++ app/src/main/java/moonu/com/ehhs/ehMain.java
@@ -4,6 +4,7 @@ import android.content.Intent;
 import android.net.Uri;
 import android.os.Bundle;
 import android.support.v7.app.AppCompatActivity;
+import android.support.v7.widget.CardView;
 import android.view.View;
 import android.widget.Button;
 
@@ -18,25 +19,28 @@ public class ehMain extends AppCompatActivity {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
 
-        findViewById(R.id.button_sch).setOnClickListener(
-                new Button.OnClickListener(){
-                    public void onClick(View v){
-                        Intent intent = new Intent(Intent.ACTION_VIEW, Uri.parse("http://eunhaeng.hs.kr/?_page=51"));
+        CardView cardView1 = findViewById(R.id.cardview_info);
+        cardView1.setOnClickListener(new View.OnClickListener() {
+                    @Override
+                    public void onClick(View v) {
+                        Intent intent = new Intent(Intent.ACTION_VIEW, Uri.parse("http://eunhaeng.hs.kr"));
                         startActivity(intent);
                     }
                 }
         );
-        findViewById(R.id.button_food).setOnClickListener(
-                new Button.OnClickListener(){
-                    public void onClick(View v){
+        CardView cardView2 = findViewById(R.id.cardview_meal);
+        cardView2.setOnClickListener(new Button.OnClickListener() {
+                    @Override
+                    public void onClick(View v) {
                         Intent intent = new Intent(getApplicationContext(),MainActivity.class);
                         startActivity(intent);
                     }
                 }
         );
-        findViewById(R.id.button_setting).setOnClickListener(
-                new Button.OnClickListener(){
-                    public void onClick(View v){
+        CardView cardView3 = findViewById(R.id.cardview_setting);
+        cardView3.setOnClickListener(new Button.OnClickListener() {
+                        @Override
+                        public void onClick(View v) {
                         Intent intent = new Intent(getApplicationContext(),SettingActivity.class);
                         startActivity(intent);
                     }
